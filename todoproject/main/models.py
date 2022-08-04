fec5b1ff705f135567cdbc941cc858168598efdb
DowranRowshenow <dowranrowshenow3d@gmail.com>
2022-08-05 01:24:55 +0500

    Updated

--- todoproject/main/models.py
+++ todoproject/main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Todo(models.Model):
 	title = models.CharField(max_length=50)
-	content = models.TextField(max_length=255)
+	content = models.TextField(max_length=255, null=True, blank=True)
 	owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 	is_favorite = models.BooleanField(default=False)
 	is_completed = models.BooleanField(default=False)
